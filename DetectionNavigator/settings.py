8b2cba79f2453f84c6c22756e649c95b452ebf56
madhavbhatt <madhav.bhatt73@gmail.com>
2021-05-25 23:27:01 -0700

    Adding Authentication

--- DetectionNavigator/settings.py
+++ DetectionNavigator/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'DetectionChart',
     'sslserver',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
